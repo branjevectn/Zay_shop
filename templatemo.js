52799973374a3ccf67f1170c1e95cc49d1734139
branjevectn <3105729+branjevectn@users.noreply.github.com>
2024-08-11 10:38:20 +0500

    Update templatemo.js

--- templatemo.js
+++ templatemo.js
@@ -1,57 +1,10 @@
-/*
+fn helper() -> Box<i32> {
+    let three = Box::new(3);
+    three // Ownership is transferred implicitly
+}
 
-TemplateMo 559 Zay Shop
-
-https://templatemo.com/tm-559-zay-shop
-
-*/
-
-'use strict';
-$(document).ready(function() {
-
-    // Accordion
-    var all_panels = $('.templatemo-accordion > li > ul').hide();
-
-    $('.templatemo-accordion > li > a').click(function() {
-        console.log('Hello world!');
-        var target =  $(this).next();
-        if(!target.hasClass('active')){
-            all_panels.removeClass('active').slideUp();
-            target.addClass('active').slideDown();
-        }
-      return false;
-    });
-    // End accordion
-
-    // Product detail
-    $('.product-links-wap a').click(function(){
-      var this_src = $(this).children('img').attr('src');
-      $('#product-detail').attr('src',this_src);
-      return false;
-    });
-    $('#btn-minus').click(function(){
-      var val = $("#var-value").html();
-      val = (val=='1')?val:val-1;
-      $("#var-value").html(val);
-      $("#product-quanity").val(val);
-      return false;
-    });
-    $('#btn-plus').click(function(){
-      var val = $("#var-value").html();
-      val++;
-      $("#var-value").html(val);
-      $("#product-quanity").val(val);
-      return false;
-    });
-    $('.btn-size').click(function(){
-      var this_val = $(this).html();
-      $("#product-size").val(this_val);
-      $(".btn-size").removeClass('btn-secondary');
-      $(".btn-size").addClass('btn-success');
-      $(this).removeClass('btn-success');
-      $(this).addClass('btn-secondary');
-      return false;
-    });
-    // End roduct detail
-
-});
+fn main() {
+    // Acquire ownership of the return value
+    let my_three = helper();
+    println!("{}",my_three);
+}
