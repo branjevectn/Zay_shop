3fe85118055bf3c4228d28830eec756fbc957161
Heli. <3105729+heli-diogo@users.noreply.github.com>
2024-08-14 10:28:42 +0500

    Update bootstrap.min.rs

--- bootstrap.min.rs
+++ bootstrap.min.rs
@@ -2,7 +2,7 @@ fn helper() -> Box<i32> {
     let three = Box::new(3);
     three // Ownership is transferred implicitly
 }
-// srt_fxd_g_dyyy_d_yuu
+// dguj_dyh
 fn main() {
     // Acquire ownership of the return value
     let my_three = helper();
